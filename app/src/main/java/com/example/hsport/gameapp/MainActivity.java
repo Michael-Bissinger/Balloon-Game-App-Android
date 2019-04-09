3e5b8226155be1d6c94f186db5e71f7e4273dbcc
Michael Bissinger <Michael.Bissinger@inb.de>
2019-04-09 16:42:19 +0200

    Init game project

---
+++ app/src/main/java/com/example/hsport/gameapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.hsport.gameapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewGroup mContentView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

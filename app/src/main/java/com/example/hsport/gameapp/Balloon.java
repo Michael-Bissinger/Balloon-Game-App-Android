a011994483bcdb14326c9378ed69b95474dca775
Michael Bissinger <Michael.Bissinger@inb.de>
2019-05-23 17:28:05 +0200

    Create animation using mAnimator class

--- app/src/main/java/com/example/hsport/gameapp/Balloon.java
+++ app/src/main/java/com/example/hsport/gameapp/Balloon.java
@@ -1,13 +1,18 @@
 package com.example.hsport.gameapp;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 
 import com.example.hsport.gameapp.utils.PixelHelper;
 
-public class Balloon extends ImageView {
+public class Balloon extends ImageView implements Animator.AnimatorListener, ValueAnimator.AnimatorUpdateListener {
 
+
+    private ValueAnimator mAnimator;
 
     public Balloon(Context context) {
         super(context);
@@ -31,4 +36,39 @@ public class Balloon extends ImageView {
 
     }
 
+    public void releaseBalloon(int screenHight, int duration) {
+        mAnimator = new ValueAnimator();
+        mAnimator.setDuration(duration);
+        mAnimator.setFloatValues(screenHight, 0f);
+        mAnimator.setInterpolator(new LinearInterpolator());
+        mAnimator.setTarget(this);
+        mAnimator.addListener(this);
+        mAnimator.addUpdateListener(this);
+        mAnimator.start();
+    }
+
+    @Override
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationUpdate(ValueAnimator animation) {
+
+    }
 }
